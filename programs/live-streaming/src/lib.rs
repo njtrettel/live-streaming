741fce126712c7ab19bbc58b2c7d2d2e020a4138
Nicolas Trettel <njtret0815@gmail.com>
2023-03-21 21:34:18 -0400

    fix tabs

--- programs/live-streaming/src/lib.rs
+++ programs/live-streaming/src/lib.rs
@@ -4,11 +4,11 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod live_streaming {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
