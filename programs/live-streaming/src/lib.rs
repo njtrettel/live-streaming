3cf5f73553cf2d4c5a4791a4388be78e85c9d086
Nicolas Trettel <njtret0815@gmail.com>
2023-04-02 18:56:55 -0400

    stub out live streaming contract

--- programs/live-streaming/src/lib.rs
+++ programs/live-streaming/src/lib.rs
@@ -6,10 +6,13 @@ declare_id!("5MXnC6dAHooETyUNL2toGukFCPsVZ7sxDKiRCrij6sMS");
 pub mod live_streaming {
   use super::*;
 
-  pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+  pub fn initialize_live_stream(ctx: Context<InitializeLiveStream>) -> Result<()> {
     Ok(())
   }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeLiveStream {
+  #[account(mut)]
+  pub owner: Signer<'info>
+}
