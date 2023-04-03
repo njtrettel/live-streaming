feafbf23a08a4190a4a75735db7541dec0b890ac
Nicolas Trettel <njtret0815@gmail.com>
2023-04-02 20:30:31 -0400

    add notes

--- programs/live-streaming/src/lib.rs
+++ programs/live-streaming/src/lib.rs
@@ -11,8 +11,25 @@ pub mod live_streaming {
   }
 }
 
+
+// - init live stream with a PDA. seeds should be ["live_stream", creator.key().as_ref(), id], where
+//   `id` is a provided String value
+// - live stream PDA account stores the creator public key, so it can be verified for payment
 #[derive(Accounts)]
-pub struct InitializeLiveStream {
+pub struct InitializeLiveStream<'info> {
   #[account(mut)]
-  pub owner: Signer<'info>
+  pub creator: Signer<'info>
+}
+
+// - user requests access to stream via an instruction
+// - the instruction takes in the creator public key and finds the PDA with ["live_stream, creator.key().as_ref(), id"]
+//   where the id is the String stream id
+// - the instruction transfers money from the user to the creator, and only then does it create an account
+//     - is it possible to conditionally create an account? if not, then set some "paid" value to true/false based on token transfer
+//     - maybe transfer to the stream PDA instead as an escrow? i think only our program would allow that transfer since it's
+//       the only one that can sign for the PDA
+#[derive(Accounts)]
+pub struct UserLiveStreamAccess<'info> {
+  #[account(mut)]
+  pub user: Signer<'info>
 }
